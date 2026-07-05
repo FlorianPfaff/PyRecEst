c50207db3ec1bf3af95953ccb20d831ad8a48a97
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-05 14:30:33 +0200

    Normalize JAX FFT array axes

--- src/pyrecest/_backend/jax/fft.py
+++ src/pyrecest/_backend/jax/fft.py
@@ -29,6 +29,26 @@ def _normalize_real_fft_axis(axis):
     return axis
 
 
+def _normalize_fft_axes(axes):
+    """Return Python ``int`` tuples for integer array FFT axis sequences."""
+    if axes is None:
+        return None
+    if isinstance(axes, _jnp.ndarray):
+        axes_array = _np.asarray(axes)
+    elif isinstance(axes, _np.ndarray):
+        axes_array = axes
+    else:
+        return axes
+
+    if axes_array.ndim == 0:
+        return axes
+    if axes_array.ndim == 1 and _np.issubdtype(
+        axes_array.dtype, _np.integer
+    ) and not _np.issubdtype(axes_array.dtype, _np.bool_):
+        return tuple(int(axis) for axis in axes_array.tolist())
+    return axes
+
+
 def rfft(a, n=None, axis=-1, norm=None):
     return _fft.rfft(_jnp.asarray(a), n=n, axis=_normalize_real_fft_axis(axis), norm=norm)
 
@@ -38,16 +58,16 @@ def irfft(a, n=None, axis=-1, norm=None):
 
 
 def fftn(a, s=None, axes=None, norm=None):
-    return _fft.fftn(_jnp.asarray(a), s=s, axes=axes, norm=norm)
+    return _fft.fftn(_jnp.asarray(a), s=s, axes=_normalize_fft_axes(axes), norm=norm)
 
 
 def ifftn(a, s=None, axes=None, norm=None):
-    return _fft.ifftn(_jnp.asarray(a), s=s, axes=axes, norm=norm)
+    return _fft.ifftn(_jnp.asarray(a), s=s, axes=_normalize_fft_axes(axes), norm=norm)
 
 
 def fftshift(x, axes=None):
-    return _fft.fftshift(_jnp.asarray(x), axes=axes)
+    return _fft.fftshift(_jnp.asarray(x), axes=_normalize_fft_axes(axes))
 
 
 def ifftshift(x, axes=None):
-    return _fft.ifftshift(_jnp.asarray(x), axes=axes)
+    return _fft.ifftshift(_jnp.asarray(x), axes=_normalize_fft_axes(axes))
