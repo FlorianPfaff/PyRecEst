80095f4362d36546f91dab1ef8fbe8091769d5c7
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-07 17:45:06 +0200

    Use deterministic coefficients in low-rank filter tests

--- tests/filters/test_low_rank_hypertoroidal_fourier_filter.py
+++ tests/filters/test_low_rank_hypertoroidal_fourier_filter.py
@@ -4,7 +4,6 @@ import numpy as np
 import numpy.testing as npt
 
 import pyrecest.backend
-from pyrecest.distributions import WrappedNormalDistribution
 from pyrecest.distributions.hypertorus.hypertoroidal_fourier_distribution import (
     HypertoroidalFourierDistribution,
 )
@@ -14,6 +13,14 @@ from pyrecest.filters.low_rank_hypertoroidal_fourier_filter import (
 )
 
 
+def _identity_coefficients_1d(scale=1.0):
+    coeff = np.zeros(5, dtype=np.complex128)
+    coeff[2] = 1.0 / (2.0 * np.pi)
+    coeff[1] = scale * (0.01 + 0.02j)
+    coeff[3] = np.conjugate(coeff[1])
+    return coeff
+
+
 @unittest.skipIf(
     pyrecest.backend.__backend_name__ != "numpy",  # pylint: disable=no-member
     reason="Low-rank Fourier prototype is NumPy-only",
@@ -21,21 +28,13 @@ from pyrecest.filters.low_rank_hypertoroidal_fourier_filter import (
 class TestLowRankHypertoroidalFourierFilter(unittest.TestCase):
     def test_rejects_sqrt_transform(self):
         with self.assertRaises(NotImplementedError):
-            LowRankHypertoroidalFourierFilter((9,), "sqrt")
+            LowRankHypertoroidalFourierFilter((5,), "sqrt")
 
     def test_predict_identity_matches_dense_1d(self):
-        dense_filter = HypertoroidalFourierFilter((9,), "identity")
-        low_rank_filter = LowRankHypertoroidalFourierFilter((9,), "identity")
-        prior = HypertoroidalFourierDistribution.from_distribution(
-            WrappedNormalDistribution(np.array(1.0), np.array(0.5)),
-            (9,),
-            "identity",
-        )
-        noise = HypertoroidalFourierDistribution.from_distribution(
-            WrappedNormalDistribution(np.array(0.0), np.array(0.3)),
-            (9,),
-            "identity",
-        )
+        dense_filter = HypertoroidalFourierFilter((5,), "identity")
+        low_rank_filter = LowRankHypertoroidalFourierFilter((5,), "identity")
+        prior = HypertoroidalFourierDistribution(_identity_coefficients_1d(), "identity")
+        noise = HypertoroidalFourierDistribution(_identity_coefficients_1d(0.5), "identity")
         dense_filter.filter_state = prior
         low_rank_filter.filter_state = prior
         dense_filter.predict_identity(noise)
@@ -45,18 +44,10 @@ class TestLowRankHypertoroidalFourierFilter(unittest.TestCase):
         )
 
     def test_update_identity_matches_dense_1d(self):
-        dense_filter = HypertoroidalFourierFilter((9,), "identity")
-        low_rank_filter = LowRankHypertoroidalFourierFilter((9,), "identity")
-        prior = HypertoroidalFourierDistribution.from_distribution(
-            WrappedNormalDistribution(np.array(1.0), np.array(0.5)),
-            (9,),
-            "identity",
-        )
-        noise = HypertoroidalFourierDistribution.from_distribution(
-            WrappedNormalDistribution(np.array(0.0), np.array(1.0)),
-            (9,),
-            "identity",
-        )
+        dense_filter = HypertoroidalFourierFilter((5,), "identity")
+        low_rank_filter = LowRankHypertoroidalFourierFilter((5,), "identity")
+        prior = HypertoroidalFourierDistribution(_identity_coefficients_1d(), "identity")
+        noise = HypertoroidalFourierDistribution(_identity_coefficients_1d(0.5), "identity")
         dense_filter.filter_state = prior
         low_rank_filter.filter_state = prior
         dense_filter.update_identity(noise, np.array([1.5]))
