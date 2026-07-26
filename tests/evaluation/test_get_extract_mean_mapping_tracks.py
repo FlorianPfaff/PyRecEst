ae53def4ada1fc057802df667a87b3d5e632b937
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-26 14:08:30 +0200

    Test MTT mean extraction from mapping tracks

---
+++ tests/evaluation/test_get_extract_mean_mapping_tracks.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from pyrecest.evaluation.get_extract_mean import get_extract_mean
+
+
+class Track:
+    def __init__(self, mean):
+        self.mu = np.asarray(mean)
+
+
+class TrackerWithTracksAttribute:
+    def __init__(self):
+        self.tracks = {
+            "track-a": Track([1.0, 2.0]),
+            "track-b": Track([3.0, 4.0]),
+        }
+
+
+class TrackerWithGetTracks:
+    def get_tracks(self):
+        return {
+            10: Track([5.0, 6.0]),
+            20: Track([7.0, 8.0]),
+        }
+
+
+def test_mtt_mean_extracts_mapping_values_from_tracks_attribute():
+    extract_mean = get_extract_mean("euclidean", mtt_scenario=True)
+
+    means = extract_mean(TrackerWithTracksAttribute())
+
+    assert len(means) == 2
+    np.testing.assert_array_equal(means[0], [1.0, 2.0])
+    np.testing.assert_array_equal(means[1], [3.0, 4.0])
+
+
+def test_mtt_mean_extracts_mapping_values_from_get_tracks():
+    extract_mean = get_extract_mean("euclidean", mtt_scenario=True)
+
+    means = extract_mean(TrackerWithGetTracks())
+
+    assert len(means) == 2
+    np.testing.assert_array_equal(means[0], [5.0, 6.0])
+    np.testing.assert_array_equal(means[1], [7.0, 8.0])
