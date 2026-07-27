4c2ad08e5d14b4c0e1fc792307f6e6c782c97ae5
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-27 02:53:04 +0200

    Accept array-like samples in nonadditive particle prediction (#4808)
    * Accept array-like nonadditive noise samples
    
    * Test array-like nonadditive noise samples

---
+++ tests/filters/test_nonadditive_arraylike_samples.py
@@ -0,0 +1,23 @@
+import numpy.testing as npt
+
+from pyrecest.backend import array
+from pyrecest.distributions import LinearDiracDistribution
+from pyrecest.filters.euclidean_particle_filter import EuclideanParticleFilter
+
+
+def test_predict_nonlinear_nonadditive_accepts_array_like_samples_and_weights():
+    particle_filter = EuclideanParticleFilter(n_particles=3, dim=1)
+    particle_filter.filter_state = LinearDiracDistribution(
+        array([[0.0], [1.0], [2.0]])
+    )
+
+    particle_filter.predict_nonlinear_nonadditive(
+        lambda particle, noise: particle + noise,
+        samples=[[0.5], [0.5]],
+        weights=[1.0, 1.0],
+    )
+
+    npt.assert_allclose(
+        particle_filter.filter_state.d,
+        array([[0.5], [1.5], [2.5]]),
+    )
