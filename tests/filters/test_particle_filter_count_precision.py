c686c9696e4a9f6ac58f64e0d24ba2ef6e6a0823
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-28 20:06:39 +0200

    Test temporal particle count rejection

--- tests/filters/test_particle_filter_count_precision.py
+++ tests/filters/test_particle_filter_count_precision.py
@@ -1,5 +1,6 @@
 from fractions import Fraction
 
+import numpy as np
 import pytest
 from pyrecest.filters.euclidean_particle_filter import EuclideanParticleFilter
 from pyrecest.filters.hypertoroidal_particle_filter import _validate_positive_integer
@@ -26,3 +27,27 @@ def test_particle_filter_count_validation_is_exact(validator):
         validator(rounded_half_integer)
 
     assert validator(exact_large_integer) == 2**53 + 1
+
+
+@pytest.mark.parametrize(
+    "temporal_value",
+    [
+        np.timedelta64(3, "ns"),
+        np.datetime64("1970-01-01T00:00:00.000000003"),
+        np.array(np.timedelta64(3, "ns"), dtype=object),
+        np.array(
+            np.datetime64("1970-01-01T00:00:00.000000003"), dtype=object
+        ),
+    ],
+    ids=["timedelta", "datetime", "object-timedelta", "object-datetime"],
+)
+@pytest.mark.parametrize(
+    "validator",
+    [_validate_euclidean, _validate_hypertoroidal],
+    ids=["euclidean", "hypertoroidal"],
+)
+def test_particle_filter_count_validation_rejects_temporal_scalars(
+    validator, temporal_value
+):
+    with pytest.raises(ValueError, match="positive integer"):
+        validator(temporal_value)
