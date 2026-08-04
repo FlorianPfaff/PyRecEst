da97971334865cd563bb4e052a06d6956296efea
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-08-05 00:51:07 +0800

    Test asymmetric Gaussian hypothesis covariance rejection

--- tests/filters/test_gaussian_hypothesis_covariance_validation.py
+++ tests/filters/test_gaussian_hypothesis_covariance_validation.py
@@ -19,6 +19,34 @@ class GaussianHypothesisCovarianceValidationTest(unittest.TestCase):
                 covariance=np.array([[1.0, 2.0], [2.0, 1.0]]),
             )
 
+    def test_rejects_extreme_asymmetry_without_silent_symmetrization(self):
+        maximum = np.finfo(float).max
+        covariance = np.array([[1.0, maximum], [-maximum, 1.0]])
+        previous_settings = np.seterr(all="raise")
+        try:
+            with self.assertRaisesRegex(ValueError, "covariance must be symmetric"):
+                WeightedGaussianHypothesis(
+                    mean=np.array([0.0, 0.0]),
+                    covariance=covariance,
+                )
+        finally:
+            np.seterr(**previous_settings)
+
+    def test_accepts_roundoff_level_asymmetry_and_stores_symmetric_covariance(self):
+        covariance = np.array([[1.0, 0.5 + 5e-11], [0.5, 1.0]])
+
+        hypothesis = WeightedGaussianHypothesis(
+            mean=np.array([0.0, 0.0]),
+            covariance=covariance,
+        )
+
+        expected = 0.5 * (covariance + covariance.T)
+        np.testing.assert_array_equal(hypothesis.covariance, expected)
+        np.testing.assert_array_equal(
+            hypothesis.covariance,
+            hypothesis.covariance.T,
+        )
+
     def test_accepts_positive_semidefinite_covariance(self):
         hypothesis = WeightedGaussianHypothesis(
             mean=np.array([0.0, 0.0]),
