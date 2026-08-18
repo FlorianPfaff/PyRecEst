07b345c249a47739d3489849f6cea730f07e84d6
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-08-18 17:11:55 +0800

    Test hyperhemispherical grid state ownership

---
+++ tests/filters/test_hyperhemispherical_grid_filter_state_ownership.py
@@ -0,0 +1,26 @@
+import copy
+import unittest
+
+import numpy.testing as npt
+import pyrecest.backend
+
+from pyrecest.filters.hyperhemispherical_grid_filter import (
+    HyperhemisphericalGridFilter,
+)
+
+
+@unittest.skipIf(
+    pyrecest.backend.__backend_name__ == "jax",  # pylint: disable=no-member
+    reason="Not supported on JAX backend",
+)
+class TestHyperhemisphericalGridFilterStateOwnership(unittest.TestCase):
+    def test_filter_state_assignment_does_not_alias_caller_distribution(self):
+        grid_filter = HyperhemisphericalGridFilter(50, 2)
+        assigned_state = copy.deepcopy(grid_filter.filter_state)
+        expected_values = copy.deepcopy(assigned_state.grid_values)
+
+        grid_filter.filter_state = assigned_state
+
+        self.assertIsNot(grid_filter.filter_state, assigned_state)
+        assigned_state.grid_values = assigned_state.grid_values * 2.0
+        npt.assert_allclose(grid_filter.filter_state.grid_values, expected_values)
